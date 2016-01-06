40311ce54d1e0b33fc720193786dab4cc951ed37
ahhnupsingh <anup.singh2071@gmail.com>
2016-01-06 13:51:10 +0545

    Initial Commit

---
+++ app/src/main/java/com/example/anup/details/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.anup.details;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
